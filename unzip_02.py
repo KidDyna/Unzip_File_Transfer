4d9c28dab1e78532126b4dd07a9f1f0a471e55bb
KidDyna <62221991+KidDyna@users.noreply.github.com>
2022-05-12 21:49:08 -0700

    Update to path from full path to current directory path

--- unzip_02.py
+++ unzip_02.py
@@ -5,7 +5,7 @@ import os.path
 import time
 
 # Create a ZipFile Object and load sample.zip in it
-with zipfile.ZipFile(r'C:\Users\kiddy\Documents\GitHub\unzip_file_transfer\Unzip_File_Transfer\zipData\zip-codes-database-STANDARD-csv.zip', 'r') as zipObj:
+with zipfile.ZipFile(r'zipData\zip-codes-database-STANDARD-csv.zip', 'r') as zipObj:
     # Get a list of all archived file names from the zip
     listOfFileNames = zipObj.namelist()
     # Iterate over the file names
@@ -13,11 +13,11 @@ with zipfile.ZipFile(r'C:\Users\kiddy\Documents\GitHub\unzip_file_transfer\Unzip
         # Check filename endswith csv
         if fileName.endswith('.csv'):
             # Extract a single file from zip
-            zipObj.extract(fileName, r'C:\Users\kiddy\Documents\GitHub\unzip_file_transfer\Unzip_File_Transfer\zipData\extracts')
+            zipObj.extract(fileName, r'zipData\extracts')
 
 ############ STILL NEED TO FIGURE OUT IDENTIFYING THE DELUXE AREA CODE DB ZIP WITH A WILDCARD INSTEAD OF YYYY-MM- ##############
 
-with zipfile.ZipFile(r'C:\Users\kiddy\Documents\GitHub\unzip_file_transfer\Unzip_File_Transfer\zipData\2022-03-DeluxeAreaCodeDatabase-csv.zip', 'r') as zipObj:
+with zipfile.ZipFile(r'zipData\2022-03-DeluxeAreaCodeDatabase-csv.zip', 'r') as zipObj:
     # Get a list of all archived file names from the zip
     listOfFileNames = zipObj.namelist()
     # Iterate over the file names
@@ -25,11 +25,11 @@ with zipfile.ZipFile(r'C:\Users\kiddy\Documents\GitHub\unzip_file_transfer\Unzip
         # Check filename endswith csv
         if fileName.endswith('.csv'):
             # Extract a single file from zip
-            zipObj.extract(fileName, r'C:\Users\kiddy\Documents\GitHub\unzip_file_transfer\Unzip_File_Transfer\zipData\extracts')
+            zipObj.extract(fileName, r'zipData\extracts')
 
 # Renames removes date prefix from Deluxe Area Code file
 from glob import glob
-path = r'C:\Users\kiddy\Documents\GitHub\unzip_file_transfer\Unzip_File_Transfer\zipData\extracts'
+path = r'zipData\extracts'
 f = glob(os.path.join(path, "*DeluxeAreaCodeDatabase.csv"))[0]
 os.rename(f, os.path.join(path, "DeluxeAreaCodeDatabase.csv"))
 
@@ -40,8 +40,8 @@ print(dir_list)
 
 # Moves files to new folder
 
-shutil.copytree(r"C:\Users\kiddy\Documents\GitHub\unzip_file_transfer\Unzip_File_Transfer\zipData\extracts",r"C:\Users\kiddy\Documents\GitHub\unzip_file_transfer\Unzip_File_Transfer\zipData\new_path_fake_NDM_directory_for_file_transfer", dirs_exist_ok=True)
-shutil.copytree(r"C:\Users\kiddy\Documents\GitHub\unzip_file_transfer\Unzip_File_Transfer\zipData\extracts",r"C:\Users\kiddy\Documents\GitHub\unzip_file_transfer\Unzip_File_Transfer\zipData\Dialer_Archive_For_Zip_Files", dirs_exist_ok=True)
+shutil.copytree(r"zipData\extracts",r"zipData\new_path_fake_NDM_directory_for_file_transfer", dirs_exist_ok=True)
+shutil.copytree(r"zipData\extracts",r"zipData\Dialer_Archive_For_Zip_Files", dirs_exist_ok=True)
 
 # Adds Month / Year prefix to files for Archive purposes
 
@@ -49,7 +49,7 @@ from glob import glob
 from datetime import datetime
 
 date = datetime.now().strftime("%Y%m")
-path = r'C:\Users\kiddy\Documents\GitHub\unzip_file_transfer\Unzip_File_Transfer\zipData\Dialer_Archive_For_Zip_Files'
+path = r'zipData\Dialer_Archive_For_Zip_Files'
 f = glob(os.path.join(path, "DeluxeAreaCodeDatabase.csv"))[0]
 os.rename(f, os.path.join(path, f"{date}_DeluxeAreaCodeDatabase.csv"))
 f = glob(os.path.join(path, "zip-codes-database-STANDARD.csv"))[0]
