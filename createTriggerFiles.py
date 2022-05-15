f8d7caf578953dd8f27c7197ec0d7783933147f7
KidDyna <62221991+KidDyna@users.noreply.github.com>
2022-05-15 11:48:16 -0700

    trigger happy

--- createTriggerFiles.py
+++ createTriggerFiles.py
@@ -1,11 +1,11 @@
 import os.path
 import time
 
-while not os.path.exists("zipData/extracts/trigger1.txt"):
+while not os.path.exists("zipData/new_path_fake_NDM_directory_for_file_transfer/DeluxeAreaCodeDatabase.csv"):
     time.sleep(1)
 
-if os.path.isfile("zipData/extracts/trigger1.txt"):
+if os.path.isfile("zipData/new_path_fake_NDM_directory_for_file_transfer/DeluxeAreaCodeDatabase.csv"):
     open("zipData/new_path_fake_NDM_directory_for_file_transfer/trigger1.txt","w+")
-    open("zipData/new_path_fake_NDM_directory_for_file_transfer/trigger2.txt", "w+")
+    open("zipData/new_path_fake_NDM_directory_for_file_transfer/trigger2.txt","w+")
 else:
-    raise ValueError("%s isn't a file!" % "zipData/extracts/trigger1.txt")
+    raise ValueError("%s isn't a file!" % "zipData/new_path_fake_NDM_directory_for_file_transfer/DeluxeAreaCodeDatabase.csv")
