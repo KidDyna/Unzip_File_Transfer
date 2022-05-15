0c896c220c6d1be8bc06a6abd7f6085db64bef9f
KidDyna <62221991+KidDyna@users.noreply.github.com>
2022-05-14 23:05:37 -0700

    Trying to implement Sonic's suggestions with failure all around.

--- ifFileName.py
+++ ifFileName.py
@@ -1,6 +1,14 @@
 import os
+import re
+import zipfile
 
-filenames = os.listdir("zipData")
+filenames = os.listdir('zipData\.')
 for filename in filenames:
-     if "DeluxeAreaCodeDatabase-csv.zip".lower() in filename.lower():
-         print(filename)
+    if re.findall(r"DeluxeAreaCodeDatabase-csv", filename):
+        print(filename)
+        break
+
+    if not filename:
+        print("No file found")
+
+    with zipfile.ZipFile(fileName, 'r')
